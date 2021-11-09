69ba5cf90122e523af90f51e84d808d3de644b7e
Tamil0105 <tamil16399@gmail.com>
2021-11-09 08:26:54 +0530

    new test cases added

---
+++ src/test/java/com/test/demo/TestingGit.java
@@ -0,0 +1,16 @@
+package com.test.demo;
+
+public class TestingGit {
+
+	public void atest() {
+		
+		System.out.println("testing");
+
+	}
+
+	public void btest() {
+		System.out.println("git testing");
+
+	}
+
+}
