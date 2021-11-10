573a113e8e4bfb53c2ff6fd0a79f07f849692686
Tamil0105 <93960209+Tamil0105@users.noreply.github.com>
2021-11-10 08:12:59 +0530

    Update TestingGit.java

--- src/test/java/com/test/demo/TestingGit.java
+++ src/test/java/com/test/demo/TestingGit.java
@@ -17,4 +17,14 @@ public class TestingGit {
 		System.out.println("git testing for c");
 
 	}
+	
+	public void dtest() {
+		System.out.println("git testing for d");
+
+	}
+	
+	public void etest() {
+		System.out.println("git testing for e");
+
+	}
 }
